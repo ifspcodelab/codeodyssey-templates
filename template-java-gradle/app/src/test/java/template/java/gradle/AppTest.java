3476e973f2f2e19471944339c9afeba38b8df812
Domingos Latorre <domingos.latorre@gmail.com>
2023-11-30 14:14:55 -0300

    initial commit

---
+++ template-java-gradle/app/src/test/java/template/java/gradle/AppTest.java
@@ -0,0 +1,10 @@
+package template.java.gradle;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAGreeting2() {
+        assertEquals(1, 1);
+    }
+}
